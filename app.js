3b552f1128710df4024c8a477f15b9cb81c4bf41
Amy Friedman <afriedman886@gmail.com>
2017-06-06 20:58:23 -0500

    connect mongo db to server

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
-const MongoClient = require('mongodb').MongoClient
 
 app.listen(3000, function() {
   console.log('listening on 3000')
 })
 
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db.close();
+});
