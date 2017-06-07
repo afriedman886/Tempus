4979537d22e78ba329d4b03fcb66e310d1430fa2
Amy Friedman <afriedman886@gmail.com>
2017-06-07 13:56:06 -0500

    replace http object with express; remove server_connect practice file

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-var http = require('http');
+// var http = require('http');
 var port = 1337;
+var express = require('express');
+var app = express();
 
-http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  });
+// http.createServer(function(req, res) {
+//   res.writeHead(200, {
+//     'Content-Type': 'text/plain'
+//   });
 
-  res.end("Hello Amy, you've stumbled on the simplest web server ever.");
+//   res.end("Hello Amy, you've stumbled on the simplest web server ever.");
 
-}).listen(port);
+// }).listen(port);
 
-console.log('Our awesome web server running at http://localhost:' + port);
+// console.log('Our awesome web server running at http://localhost:' + port);
+
+app.use('/', function(req, res) {
+  res.send('Hello World');
+});
+
+app.listen(port);
+
+console.log('Server running at http://localhost:' + port);
