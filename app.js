81a5e1d5d3ae64d07f445e6537a9181ee94e6114
Amy Friedman <afriedman886@gmail.com>
2017-06-06 21:10:39 -0500

    create functions to add new documents to database and search database for all documents

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.listen(3000, function() {
   console.log('listening on 3000')
-})
+});
 
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
@@ -16,5 +16,36 @@ MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
 
-  db.close();
+  insertDocuments(db, function() {
+    findDocuments(db, function() {
+      db.close();
+    });
+  });
 });
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
