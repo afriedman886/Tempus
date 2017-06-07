65357d7d8f2812e87140ff2e5e8efc1810f4ca78
Amy Friedman <afriedman886@gmail.com>
2017-06-07 14:20:07 -0500

    test routes/controller for get request to index

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // var http = require('http');
 var port = 1337;
-var express = require('express');
+var express = require('./app/config/express');
 var app = express();
 
 // http.createServer(function(req, res) {
@@ -14,10 +14,8 @@ var app = express();
 
 // console.log('Our awesome web server running at http://localhost:' + port);
 
-app.use('/', function(req, res) {
-  res.send('Hello World');
-});
 
 app.listen(port);
+module.exports = app;
 
 console.log('Server running at http://localhost:' + port);
