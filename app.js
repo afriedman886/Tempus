a5e94e317202c24970457bef1d622176e359f3e6
Amy Friedman <afriedman886@gmail.com>
2017-06-06 21:15:59 -0500

    create function to update and remove documents from database

--- app.js
+++ app.js
@@ -16,9 +16,13 @@ MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
 
-  insertDocuments(db, function() {
-    findDocuments(db, function() {
-      db.close();
+  findDocuments(db, function() {
+    insertDocuments(db, function() {
+      updateDocument(db, function() {
+        removeDocument(db, function() {
+          db.close();
+        });
+      });
     });
   });
 });
@@ -47,5 +51,30 @@ var findDocuments = function(db, callback) {
     console.log("Found the following records");
     console.log(docs)
     callback(docs);
+  });
+}
+
+var updateDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });
+}
+
+var removeDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Delete document where a is 3
+  collection.deleteOne({ a : 3 }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
   });
 }
