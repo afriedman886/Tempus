5850867d51831d3f3a7427cdc6b8e5cd78e47ee8
Amy Friedman <afriedman886@gmail.com>
2017-06-07 20:32:10 -0500

    add controller and route to show individual patient

--- app/controllers/patients.server.controller.js
+++ app/controllers/patients.server.controller.js
@@ -11,6 +11,26 @@ exports.list = function(req, res, next) {
     });
 };
 
+exports.read = function(req, res) {
+    res.json(req.patient);
+};
+
+exports.patientByID = function(req, res, next, id) {
+    Patient.findOne({
+            _id: id
+        },
+        function(err, patient) {
+            if (err) {
+                return next(err);
+            }
+            else {
+                req.patient = patient;
+                next();
+            }
+        }
+    );
+};
+
 exports.create = function(req, res, next) {
     var patient = new Patient(req.body);
     patient.save(function(err) {
