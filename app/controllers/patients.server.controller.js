b8ff04c9946f66066a41c47b8c391f3e80ad8b16
Amy Friedman <afriedman886@gmail.com>
2017-06-07 23:18:50 -0500

    add login/logout functions to patient controller

--- app/controllers/patients.server.controller.js
+++ app/controllers/patients.server.controller.js
@@ -1,4 +1,44 @@
 var Patient = require('mongoose').model('Patient');
+var passport = require('passport');
+
+var getErrorMessage = function(err) {
+    var message = '';
+    if (err.code) {
+        switch (err.code) {
+            case 11000:
+            case 11001:
+                message = 'Username already exists';
+                break;
+            default:
+                message = 'Something went wrong';
+        }
+    }
+    else {
+        for (var errName in err.errors) {
+            if (err.errors[errName].message)
+                message = err.errors[errName].message;
+        }
+    }
+
+    return message;
+};
+
+exports.renderLogin = function(req, res, next) {
+    if (!req.patient) {
+        res.render('login', {
+            title: 'Log-in Form',
+            messages: req.flash('error') || req.flash('info')
+        });
+    }
+    else {
+        return res.redirect('/');
+    }
+};
+
+exports.logout = function(req, res) {
+    req.logout();
+    res.redirect('/');
+};
 
 exports.list = function(req, res, next) {
     Patient.find({}, function(err, patients) {
