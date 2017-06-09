9fb69de527b61a01adf2d8d65be409d73eba4e69
Amy Friedman <afriedman886@gmail.com>
2017-06-09 09:34:26 -0500

    create ejs templating for patient list, remove non-applicable comments from config files

--- app/config/express.js
+++ app/config/express.js
@@ -28,7 +28,6 @@ module.exports = function() {
     app.set('views', 'app/views');
     app.set('view engine', 'ejs');
 
-    // require('../routes/index.server.routes.js')(app);
     require('../routes/users.server.routes.js')(app);
 
     app.use(express.static('../../public'));
